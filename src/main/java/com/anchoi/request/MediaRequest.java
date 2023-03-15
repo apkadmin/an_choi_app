1235e3cb1fa06f21f538f9fe5c67cd255216f23e
vinci <tuyendt@itsol.vn>
2023-03-16 01:14:24 +0700

    upload media api

---
+++ src/main/java/com/anchoi/request/MediaRequest.java
@@ -0,0 +1,10 @@
+package com.anchoi.request;
+
+import lombok.Data;
+
+@Data
+public class MediaRequest {
+    private String typeMedia;
+    private String idRefer;
+    private String type;
+}
---
+++ src/main/java/com/anchoi/service/MediaService.java
@@ -0,0 +1,23 @@
+package com.anchoi.service;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+import com.anchoi.models.Media;
+import com.anchoi.request.MediaRequest;
+import org.springframework.core.io.Resource;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface MediaService {
+    public void init();
+
+    public List<Media> save(MediaRequest mediaRequest, MultipartFile[] medias);
+
+    public Resource load(String filename);
+
+    public void deleteAll();
+
+    public Stream<Path> loadAll();
+}
+
