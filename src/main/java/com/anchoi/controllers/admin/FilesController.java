19e2d5be111424ec25b20fef6af900cd69c7d285
vinci <tuyendt@itsol.vn>
2023-03-16 08:43:59 +0700

    update path upload

--- src/main/java/com/anchoi/controllers/admin/FilesController.java
+++ src/main/java/com/anchoi/controllers/admin/FilesController.java
@@ -1,7 +1,6 @@
 package com.anchoi.controllers.admin;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import com.anchoi.config.BusinessException;
 import com.anchoi.models.Media;
@@ -13,12 +12,8 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
-import com.anchoi.payload.response.MessageResponse;
-
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
