072d02fb033b5021aa18dc15f5c90b95eb92dd24
annv <annv@itsol.vn>
2023-08-03 19:55:31 +0700

    [annv] update router

---
+++ src/main/java/com/anchoi/models/MediaDesRequest.java
@@ -0,0 +1,13 @@
+package com.anchoi.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class MediaDesRequest {
+   private String id;
+   private String description;
+}
