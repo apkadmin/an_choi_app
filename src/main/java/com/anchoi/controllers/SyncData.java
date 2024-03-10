decd4c0b9fc565f01d9c73c5c6841268319710dd
apkadmin <nguyenvanan11061997@gmail.com>
2024-03-10 16:51:13 +0700

    [annv] sync data

---
+++ src/main/java/com/anchoi/controllers/SyncData.java
@@ -0,0 +1,26 @@
+package com.anchoi.controllers;
+
+import com.anchoi.entity.Post;
+import com.anchoi.service.SyncService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController()
+@RequestMapping("/api/sync")
+@RequiredArgsConstructor
+public class SyncData {
+   private final SyncService syncService;
+
+    @GetMapping
+    public ResponseEntity<String> syncPost(@RequestHeader(value = "lang", defaultValue = "vi") String lang) {
+        syncService.syncService(lang);
+        return ResponseEntity.ok().body("OK");
+    }
+
+}
---
+++ src/main/java/com/anchoi/service/SyncService.java
@@ -0,0 +1,2 @@
+package com.anchoi.service;public class SyncService {
+}
